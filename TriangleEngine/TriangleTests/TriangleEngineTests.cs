8ec7fe74c12759c794d025d41d67de7ba1739f8d
mike <michael.cwienczek@gmail.com>
2013-10-19 23:21:32 +0200

    First test

---
+++ TriangleEngine/TriangleTests/TriangleEngineTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleLive;
+using System.Linq;
+
+namespace TriangleTests
+{
+    [TestClass]
+    public class TriangleEngineTests
+    {
+        [TestMethod]
+        public void MoveMonsterToNextEmptyField()
+        {
+            TriangleEngine engine = new TriangleEngine();
+            engine.PutMonsterOnBoard(new Bear(1, 1));
+            engine.Turn();
+            Bear b = (Bear) engine.Monsters.First();
+            Assert.IsTrue(b.Pos.X == 0 || b.Pos.X == 2 || b.Pos.Y == 0 || b.Pos.Y == 2);
+        }
+    }
+}
